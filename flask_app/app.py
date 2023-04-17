acba4538977862fe39855e46a1a706e054ff0653
sairamkiran9 <sairamkiranmuppana.1999@gmail.com>
2023-04-16 23:21:23 -0400

    python server side database

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,29 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
-import json
-import os
+from user import User
 
 app = Flask(__name__)
 
-class User:
-    def __init__(self, username, password, email) -> None:
-        self.username = username
-        self.password = password
-        self.email = email
-        self.create_creds()
 
-    def create_creds(self):
-        current_path = os.getcwd()  # Get the current working directory
-        filename = 'creds.csv'
-        full_path = os.path.join(current_path, filename)
-        if not os.path.exists(full_path):
-            with open(full_path, "w+") as f:
-                f.write("username,password,email\n")
-
-    def save_details(self):
-        data = self.username + "," + self.password + "," + self.email + "\n"
-        with open("creds.csv", "a") as f:
-            f.write(data)
-        
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     msg = ''
@@ -31,8 +11,6 @@ def register():
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
-        user = User(username, password, email)
-        user.save_details()
         return redirect(url_for('message', name=username))
     elif request.method == 'POST':
         msg = 'Please fill out the form !'
@@ -50,17 +28,22 @@ def login_post():
     username = request.form["username"]
     password = request.form["password"]
     return redirect(url_for('message', name=username))
-    # return render_template("home.html", name=username)
+
 
 @app.route("/message")
 def message():
     name = request.args.get('name')
     return render_template("home.html", name=name)
 
+
 @app.route("/hello")
 def hello():
+    print(request.args.get('body'))
     return "srivennala"
 
 
 if __name__ == "__main__":
+    user = User()
+    user.create_table()
+    print(user.get_data("select * from users"))
     app.run(debug=True)
